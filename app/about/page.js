90821b13a0e3127b7c772c1c908930817802793f
Anshul Sharma <sharma.ansh2926@gmail.com>
2024-11-10 22:41:58 +0530

    Learned about basics

---
+++ app/about/page.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const page = () => {
+    return(
+        <>
+            <h1 className="text-xl">
+                Problems solved by Next.js
+                <ul>
+                    <li>Full Stack Solution</li>
+                    <li>File Based Routing</li>
+                    <li>Additional features like router from react/navigation</li>
+                    <li>Optimized rendering</li>
+                </ul>
+            </h1>
+        </>
+    )
+}
+
+
+export default page;
